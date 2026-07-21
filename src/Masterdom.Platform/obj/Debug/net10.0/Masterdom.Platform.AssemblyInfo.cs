49a8274ba01bf468374522d63dac7da5ad062579
Kapil Yadav <kady@kady.local>
2026-07-21 16:20:37 +0530

    Complete ModuleLoader package

--- src/Masterdom.Platform/obj/Debug/net10.0/Masterdom.Platform.AssemblyInfo.cs
+++ src/Masterdom.Platform/obj/Debug/net10.0/Masterdom.Platform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Masterdom.Platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49d191bb24e9fd44bc44d47a1cc8dabfdd829093")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4189710edd74f6d9b9ca0e368c2456492039668")]
 [assembly: System.Reflection.AssemblyProductAttribute("Masterdom.Platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Masterdom.Platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
